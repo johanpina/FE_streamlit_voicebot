048c5ac83a823fd7ccb4d2b066f90a913cc7e39c
johanpina <johanpina1420@gmail.com>
2023-08-29 03:04:27 -0500

    adding FE and dockerFile to compile

--- Police_cat.py
+++ Police_cat.py
@@ -4,33 +4,36 @@ import argparse
 from numpy.linalg import norm
 from sentence_transformers import SentenceTransformer
 
-parser = argparse.ArgumentParser(description='Time series forecasting program')
-parser.add_argument("--f", help="Sentence to compare with", default="me colabora que los vecinos hacen mucha bulla")
-args = parser.parse_args()
 
 # Coseine similarity for sentence comparison
 def cos_sim (A,B):
   return np.dot(A,B)/(norm(A)*norm(B))
 
+
 # Base sentence for classify
 df = pd.read_excel('Policia_Cat.xlsx')
+
 sentences = [item for row in df.values for item in row]
 
-# Pretrained model on sentence similarity spanish
-model = SentenceTransformer('hiiamsid/sentence_similarity_spanish_es')
-embeddings = model.encode(sentences)
+def encode_DB(sentences):
+  model = SentenceTransformer('hiiamsid/sentence_similarity_spanish_es')
+  embeddings = model.encode(sentences)
+  return model,embeddings
 
-#Embedding on the new sentence to compare
-prueba = args.f
-input = model.encode(prueba)
-
-#Get cosine similarity of all sentences, sort and extract best 5
-similarity = [cos_sim(input,x) for x in embeddings]
-ind = np.argsort(similarity)[-5:]
-ind[:] = ind[::-1]
+def embed_sentence(model, sentence):
+  return model.encode(sentence)
 
 
-#With the most similar get the class (Column name)
-for col in df.columns:
-    if sentences[ind[0]] in df[col].values:
-        print(col)
+def best_similarity(input, embeddings,best_k:int):
+  #Get cosine similarity of all sentences, sort and extract best 5
+  similarity = [cos_sim(input,x) for x in embeddings]
+  ind = np.argsort(similarity)[-best_k:]
+  ind[:] = ind[::-1]
+  return ind
+
+def most_similar_class(ind, df, sentences):
+  #With the most similar get the class (Column name)
+  for col in df.columns:
+      if sentences[ind[0]] in df[col].values:
+          return col
+  return "No identificado"
